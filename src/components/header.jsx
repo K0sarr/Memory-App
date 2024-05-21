431997015685c690cebc562c7369145471d3ceef
K0sarr <Kosar_Abobakri@yahoo.com>
2024-05-21 18:59:27 +0200

    Capitalized names, score moved to app
    Moved the score to App.jsx to life the state up. Also capitalized names from the api. Adjust more Css; smaller screen adjustments etc.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,17 +2,20 @@ import React from "react"
 import '../styles/header.css'
 
 
-export default function Header() {
-
-
-
+export default function Header({ score, bestScore }) {
 
     return (
+        <>
+        <div className="score-title">
+        <p className="score-text">Score: {score}</p>
+        <p className="bestScore-text">Best Score: {bestScore}</p>
+        </div>
         <div className="header-title">
         <h1>Pokemon Memory Game</h1>
-        <span>You get points by clicking on an image, but don't click
+        <span>You get points by clicking on an image but don't click
             on any more than once!
         </span>
         </div>
+        </>
     )
 }
